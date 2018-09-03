c736cab19fd0b0b7213e11ef5c6fa678be177216
Jarde01 <j_arde@live.ca>
2018-09-03 13:16:38 -0500

    cleaning up a bit

--- src/Program.py
+++ src/Program.py
@@ -40,43 +40,34 @@ def create_spectrogram_helper(from_path, to_path, threadcount=4):
             wav_file_path = Path(os.getcwd(), from_path, file)
 
             # # don't create too many threads
-            # while threading.active_count() > 2:
+            # while threading.active_count() > threadcount:
             #     print("Currently: ", threading.active_count(), " threads alive")
             #     time.sleep(3)
             #
             # i = 0
             # while i < threadcount:
-            #     t = Thread(target=create_spectrogram, args=(wav_file_path, to_path))
+            #     t = Thread(target=graph_spectrogram, args=(wav_file_path, to_path))
             #     t.start()
             #     i += 1
-
-            # sound = AudioSegment.from_file(wav_file_path, format="wav")
-            # channel_count = sound.channels
-            #
-            # if channel_count is 2:
-            #     sound.set_channels(1)
-            #
-            # filename = str(wav_file_path)[:-4] + "_tests.wav"
-            # sound.export(filename, format="wav")
-            # create_spectrogram(wav_file_path, to_path)
-            print(wav_file_path)
-
-            graph_spectrogram(wav_file_path, i)
-            i += 1
+            graph_spectrogram(wav_file_path, to_path)
 
 
-def graph_spectrogram(wav_file, i):
-    sound_info, frame_rate = get_wav_info(wav_file)
-    fig = pylab.figure(num=None, frameon=False)
-    fig.set_size_inches(20, 10)
-    axis = pylab.Axes(fig, [0., 0, 1., 1.])
-    axis.set_axis_off()
-    fig.add_axes(axis)
-    # pylab.specgram(sound_info, Fs=frame_rate)
-    # larger values of NFFT will cause the middle freq to become to pronounced
-    pylab.specgram(sound_info, Fs=frame_rate, NFFT=80, noverlap=64)
-    pylab.savefig(str(i)+'_spectrogram.png')
-    pylab.close()
+def graph_spectrogram(wav_file, to_path):
+    spec_file_path = Path(to_path, str(Path(wav_file).name)[:-4] + '.png')
+    if os.path.isfile(spec_file_path):
+        print(Path(spec_file_path).name, " already exists")
+    else:
+        sound_info, frame_rate = get_wav_info(wav_file)
+        fig = pylab.figure(num=None, frameon=False)
+        fig.set_size_inches(20, 10)
+        axis = pylab.Axes(fig, [0., 0, 1., 1.])
+        axis.set_axis_off()
+        fig.add_axes(axis)
+        # pylab.specgram(sound_info, Fs=frame_rate)
+        # larger values of NFFT will cause the middle freq to become to pronounced
+        pylab.specgram(sound_info, Fs=frame_rate, NFFT=80, noverlap=64)
+        pylab.savefig()
+        pylab.close()
 
 
 def get_wav_info(wav_file):
@@ -89,43 +80,18 @@ def get_wav_info(wav_file):
     return sound_info, frame_rate
 
 
-def create_spectrogram(file_path, to_path):
-    sample_rate, samples = wavfile.read(Path(file_path))
-    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
-
-    #dBS = 10*numpy.log10(spectrogram) # converting to dB
-    #plt.pcolormesh(times, frequencies, dBS)
-
-    plt.pcolormesh(times, frequencies, spectrogram)
-    plt.imshow(spectrogram)
-    plt.show()
-    #plt.imsave()
-
-
-def split_into_chunks():
-    # t1 = Thread(target=split_songs_into_chunks, args=(music_path, mp3_split))
-    # t1.start()
-
-    t2 = Thread(target=convert_mp3_to_wav_helper, args=(mp3_split, wav_folder, 29))
-    t2.start()
-
-    # t1.join()
-    t2.join()
-
-
 def get_genre_dict():
     reader = DataReader(config)
     genres = reader.load_genres()
 
-
-    genreDict = []
+    genre_dict = []
 
     for index in range(len(genres.title)):
         tup = (genres.title[index], genres.numtracks[index])
-        genreDict += tup
+        genre_dict += tup
         #print(index, genreDict[index])
 
-    print(genreDict)
+    print(genre_dict)
 
 
-create_spectrogram_helper(wav_folder, spec_folder, 1)
+create_spectrogram_helper(wav_folder, spec_folder, threadcount=10)
